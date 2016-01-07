6716f5063508f9e95c9f0361b6a7ad06e612618f
Arun Khandavalli <akhandav@qti.qualcomm.com>
2016-01-07 19:02:47 +0530

    qcacld-2.0: Add support for BPF data offload
    Currently wlan firmware has multiple different packet filters,
    offload features such as Magic Pattern filter, MCBC filter,
    RA filter and Bitmap pattern filter.
    Firmware has to maintain different filter settings and different
    source code to compare the filters.
    
    BPF data offload is common architecture for packet filter offload.
    Implement BPF data offload in driver.
    
    Change-Id: I63a5f7222de74cbb80d656f20f42d1b8453dce26
    CRs-Fixed: 960473

--- drivers/net/wireless/qcacld-2.0/CORE/SME/inc/sme_Api.h
+++ drivers/net/wireless/qcacld-2.0/CORE/SME/inc/sme_Api.h
@@ -4094,6 +4094,10 @@ eHalStatus sme_ExtScanRegisterCallback (tHalHandle hHal,
 
 #endif /* FEATURE_WLAN_EXTSCAN */
 
+eHalStatus sme_bpf_offload_register_callback(tHalHandle hal,
+			void (*pbpf_get_offload_cb)(void *,
+			struct sir_bpf_get_offload *));
+
 #ifdef WLAN_FEATURE_ROAM_SCAN_OFFLOAD
 /* ---------------------------------------------------------------------------
     \fn sme_abortRoamScan
@@ -4508,4 +4512,7 @@ VOS_STATUS sme_is_session_valid(tHalHandle hal_handle, uint8_t session_id);
 void sme_send_disassoc_req_frame(tHalHandle hal, uint8_t session_id,
 		uint8_t *peer_mac, tANI_U16 reason, uint8_t wait_for_ack);
 
+eHalStatus sme_get_bpf_offload_capabilities(tHalHandle hal);
+eHalStatus sme_set_bpf_instructions(tHalHandle hal,
+			struct sir_bpf_set_offload *);
 #endif //#if !defined( __SME_API_H )
