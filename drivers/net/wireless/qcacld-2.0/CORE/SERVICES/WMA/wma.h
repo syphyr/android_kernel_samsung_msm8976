1593c354f57b6e32aa4c3a83dcbddbb46e7024db
Naveen Rawat <nrawat@qca.qualcomm.com>
2016-01-11 20:09:53 -0800

    qcacld-2.0: Add fine timing measurement capabilities from FW
    Add support to use fine timing measurement capabilities coming
    from the Firmware.
    Currently INI config is being used for setting initiator
    and responder roles. With this change, intersection of INI
    config and FW indicated capability will be used.
    
    Change-Id: If20bc8de2b6ec12bc638b75806e2a870a5ebc189
    CRs-Fixed: 942290

--- drivers/net/wireless/qcacld-2.0/CORE/SERVICES/WMA/wma.h
+++ drivers/net/wireless/qcacld-2.0/CORE/SERVICES/WMA/wma.h
@@ -861,6 +861,7 @@ typedef struct wma_handle {
 	uint16_t max_mgmt_tx_fail_count;
 
 	struct wma_runtime_pm_context runtime_context;
+	uint32_t fine_time_measurement_cap;
 	bool     pause_other_vdev_on_mcc_start;
 	tSirAddonPsReq psSetting;
 }t_wma_handle, *tp_wma_handle;
