7868a16bf4021f340a2731c3eef87be4049f8790
Wei Wang <weiwan@google.com>
2016-02-17 13:58:22 -0800

    ipv6: pass up EMSGSIZE msg for UDP socket in Ipv6
    In ipv4,  when  the machine receives a ICMP_FRAG_NEEDED message,  the
    connected UDP socket will get EMSGSIZE message on its next read from the
    socket.
    However, this is not the case for ipv6.
    This fix modifies the udp err handler in Ipv6 for ICMP6_PKT_TOOBIG to
    make it similar to ipv4 behavior. That is when the machine gets an
    ICMP6_PKT_TOOBIG message, the connected UDP socket will get EMSGSIZE
    message on its next read from the socket.
    
    Signed-off-by: Wei Wang <weiwan@google.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/udp.c
+++ net/ipv6/udp.c
@@ -510,6 +510,7 @@ void __udp6_lib_err(struct sk_buff *skb, struct inet6_skb_parm *opt,
 	const struct in6_addr *daddr = &hdr->daddr;
 	struct udphdr *uh = (struct udphdr*)(skb->data+offset);
 	struct sock *sk;
+	int harderr;
 	int err;
 
 	sk = __udp6_lib_lookup(dev_net(skb->dev), daddr, uh->dest,
@@ -517,26 +518,27 @@ void __udp6_lib_err(struct sk_buff *skb, struct inet6_skb_parm *opt,
 	if (sk == NULL)
 		return;
 
+	harderr = icmpv6_err_convert(type, code, &err);
+	np = inet6_sk(sk);
+
 	if (type == ICMPV6_PKT_TOOBIG) {
 		if (!ip6_sk_accept_pmtu(sk))
 			goto out;
 		ip6_sk_update_pmtu(skb, sk, info);
+		if (np->pmtudisc != IPV6_PMTUDISC_DONT)
+			harderr = 1;
 	}
 	if (type == NDISC_REDIRECT) {
 		ip6_sk_redirect(skb, sk);
 		goto out;
 	}
 
-	np = inet6_sk(sk);
-
-	if (!icmpv6_err_convert(type, code, &err) && !np->recverr)
-		goto out;
-
-	if (sk->sk_state != TCP_ESTABLISHED && !np->recverr)
-		goto out;
-
-	if (np->recverr)
+	if (!np->recverr) {
+		if (!harderr || sk->sk_state != TCP_ESTABLISHED)
+			goto out;
+	} else {
 		ipv6_icmp_error(sk, skb, err, uh->dest, ntohl(info), (u8 *)(uh+1));
+	}
 
 	sk->sk_err = err;
 	sk->sk_error_report(sk);
