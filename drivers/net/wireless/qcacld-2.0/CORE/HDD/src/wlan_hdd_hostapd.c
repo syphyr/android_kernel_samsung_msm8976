34a00d3987ccf0ab126b984c75122d1358880b76
Agrawal Ashish <ashishka@qti.qualcomm.com>
2016-03-21 17:20:02 +0530

    qcacld-2.0: Implement vendor command for ACS policy
    Add support for vendor command which informs the driver about the
    ACS policy.
    
    QCA_NL80211_VENDOR_SUBCMD_ACS_POLICY is used to inform driver about
    ACS policies.
    
    QCA_WLAN_VENDOR_ATTR_ACS_DFS_MODE attribute tells if DFS channel
    should be enabled or disabled in ACS.
    
    QCA_WLAN_VENDOR_ATTR_ACS_CHANNEL attribute provides channel number to SAP
    module to skip ACS and use this channel as operating channel.
    
    Change-Id: I1ba69ed8fdf1c56a90e857054c725b830293d6bf
    CRs-Fixed: 997433

--- drivers/net/wireless/qcacld-2.0/CORE/HDD/src/wlan_hdd_hostapd.c
+++ drivers/net/wireless/qcacld-2.0/CORE/HDD/src/wlan_hdd_hostapd.c
@@ -6593,6 +6593,7 @@ VOS_STATUS hdd_init_ap_mode( hdd_adapter_t *pAdapter )
 #ifdef WLAN_FEATURE_MBSSID
     v_CONTEXT_t pVosContext = (WLAN_HDD_GET_CTX(pAdapter))->pvosContext;
     v_CONTEXT_t sapContext=NULL;
+    enum dfs_mode mode;
 #endif
     int ret;
 
@@ -6616,6 +6617,11 @@ VOS_STATUS hdd_init_ap_mode( hdd_adapter_t *pAdapter )
     }
 
     pAdapter->sessionCtx.ap.sapContext = sapContext;
+    pAdapter->sessionCtx.ap.sapConfig.channel = pHddCtx->acs_policy.acs_channel;
+    mode = pHddCtx->acs_policy.acs_dfs_mode;
+    pAdapter->sessionCtx.ap.sapConfig.acs_dfs_mode =
+          wlan_hdd_get_dfs_mode(mode);
+
 
     status = WLANSAP_Start(sapContext);
     if ( ! VOS_IS_STATUS_SUCCESS( status ) )
