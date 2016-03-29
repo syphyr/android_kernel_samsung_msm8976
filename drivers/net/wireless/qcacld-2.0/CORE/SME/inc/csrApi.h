2471563ddd8e4367ca04b749f62ce05ae9581180
Agrawal Ashish <ashishka@qti.qualcomm.com>
2016-03-29 17:53:20 +0530

    qcacld-2.0: Implement vendor command for STA ROAM POLICY
    Add support for vendor command which informs the driver about
    sta roam policies about dfs mode and unsafe channels.
    QCA_NL80211_VENDOR_SUBCMD_STA_CONNECT_ROAM_POLICY sends
    QCA_WLAN_VENDOR_ATTR_STA_DFS_MODE &
    QCA_WLAN_VENDOR_ATTR_STA_SKIP_UNSAFE_CHAN
    attributes to driver to skip scan channels for station
    connection or roaming.
    If QCA_WLAN_VENDOR_ATTR_STA_DFS_MODE is disabled, station will skip
    dfs channels in scanning.
    If QCA_WLAN_VENDOR_ATTR_STA_SKIP_UNSAFE_CHAN is disabled, station
    will skip unsafe channel in scanning.
    
    Change-Id: I33dfa174d218a2f39fec3ffc240dad793b72b14b
    CRs-Fixed: 999169

--- drivers/net/wireless/qcacld-2.0/CORE/SME/inc/csrApi.h
+++ drivers/net/wireless/qcacld-2.0/CORE/SME/inc/csrApi.h
@@ -1104,6 +1104,30 @@ typedef struct tagCsrNeighborRoamConfigParams
 }tCsrNeighborRoamConfigParams;
 #endif
 
+/**
+ * enum sta_roam_policy_dfs_mode - state of DFS mode for STA ROME policy
+ * @CSR_STA_ROAM_POLICY_NONE: DFS mode attribute is not valid
+ * @CSR_STA_ROAM_POLICY_DFS_ENABLED:  DFS mode is enabled
+ * @CSR_STA_ROAM_POLICY_DFS_DISABLED: DFS mode is disabled
+ * @CSR_STA_ROAM_POLICY_DFS_DEPRIORITIZE: Deprioritize DFS channels in scanning
+ */
+enum sta_roam_policy_dfs_mode {
+	CSR_STA_ROAM_POLICY_NONE,
+	CSR_STA_ROAM_POLICY_DFS_ENABLED,
+	CSR_STA_ROAM_POLICY_DFS_DISABLED,
+	CSR_STA_ROAM_POLICY_DFS_DEPRIORITIZE
+};
+
+/**
+ * struct csr_sta_roam_policy_params - sta roam policy params for station
+ * @dfs_mode: tell is DFS channels needs to be skipped while scanning
+ * @skip_unsafe_channels: tells if unsafe channels needs to be skip in scanning
+ */
+struct csr_sta_roam_policy_params {
+	enum sta_roam_policy_dfs_mode dfs_mode;
+	bool skip_unsafe_channels;
+};
+
 typedef struct tagCsrConfigParam
 {
     tANI_U32 FragmentationThreshold;
@@ -1301,6 +1325,7 @@ typedef struct tagCsrConfigParam
     v_U16_t    pkt_err_disconn_th;
     int8_t    first_scan_bucket_threshold;
     bool    enable_fatal_event;
+    struct csr_sta_roam_policy_params sta_roam_policy_params;
 }tCsrConfigParam;
 
 //Tush
