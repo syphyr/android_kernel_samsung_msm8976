61627e7b5310f10aac988432b33d32a915449dd4
Bhargav Shah <c_bhargv@qti.qualcomm.com>
2016-06-16 14:35:59 +0530

    qcacld-2.0: Check consistency in throughput before setting delack
    Presently, throughput is measured in every 100ms. Delack is set to 20
    if throughput for this time duration is greater then threshold (57 Mbps).
    In the case of packet drop, tcp takes more time to rampup.
    In this change, check the throughput consistency for three second
    and then set tcp delack to 20.
    
    CRs-Fixed: 1028085
    Change-Id: I155bc141087104ad1443d250711423d5fa936254

--- drivers/net/wireless/qcacld-2.0/CORE/HDD/inc/wlan_hdd_main.h
+++ drivers/net/wireless/qcacld-2.0/CORE/HDD/inc/wlan_hdd_main.h
@@ -1727,6 +1727,7 @@ struct hdd_context_s
     unsigned long tdls_source_bitmap;
     /* tdls source timer to enable/disable TDLS on p2p listen */
     vos_timer_t tdls_source_timer;
+    uint32_t rx_high_ind_cnt;
     bool driver_being_stopped; /* Track if DRIVER STOP cmd is sent */
 };
 
