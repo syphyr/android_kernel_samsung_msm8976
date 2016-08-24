cf318dfccdddc3f915da77479e480e2a85c1323e
Wu Gao <wugao@codeaurora.org>
2016-08-24 18:15:51 +0800

    qcacld-2.0: Stop SAP and indicate event more earlier after SSR
    DUT is SAP + STA SBSC mode, it takes very long time to recover after
    SSR. There are two problems:
    
    1. It clear STA session and create new after SSR, but just clear SAP
    session. With sessionId in SAP context, it wrongly use new STA session
    to close SAP when terminate hostapd. This is the reason why host wait
    stop_bss_event for 10 seconds in cfg80211 stop ap. Set sessionId to
    invalid if adapter is SAP mode.
    
    2. In dual-wifi mode, it will start second WLAN if
    SSR and WLAN_SVC_FW_CRASHED_IND late. Send this event more earlier.
    
    Change-Id: I10a3f300ac5621463fcce4d0a5e18b2cf1cb8491
    CRs-Fixed: 1054612

--- drivers/net/wireless/qcacld-2.0/CORE/SAP/src/sapModule.c
+++ drivers/net/wireless/qcacld-2.0/CORE/SAP/src/sapModule.c
@@ -4024,3 +4024,28 @@ VOS_STATUS wlansap_set_tx_leakage_threshold(tHalHandle hal,
 	return VOS_STATUS_SUCCESS;
 }
 
+/*
+ * wlansap_set_invalid_session() - set session ID to invalid
+ * @pctx: pointer of global context
+ *
+ * This function sets session ID to invalid
+ *
+ * Return: VOS_STATUS
+ */
+VOS_STATUS
+wlansap_set_invalid_session(v_PVOID_t pctx)
+{
+	ptSapContext psapctx;
+	psapctx = VOS_GET_SAP_CB(pctx);
+	if ( NULL == psapctx) {
+		VOS_TRACE( VOS_MODULE_ID_SAP, VOS_TRACE_LEVEL_ERROR,
+			FL("Invalid SAP pointer from pctx"));
+		return VOS_STATUS_E_FAILURE;
+	}
+
+	psapctx->sessionId = CSR_SESSION_ID_INVALID;
+	psapctx->isSapSessionOpen = eSAP_FALSE;
+
+	return VOS_STATUS_SUCCESS;
+}
+
