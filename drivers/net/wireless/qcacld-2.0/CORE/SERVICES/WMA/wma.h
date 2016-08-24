73085f235bedbf536200f1b825f79c92a6eae1ca
Anurag Chouhan <achouhan@codeaurora.org>
2016-08-24 23:06:43 +0530

    qcacld-2.0: Disable RA filter when BPF is enabled
    In current implementation, RA filter is enabled irrespective of the
    BPF filter status. This change checks if BPF configuration in ini as
    well as in target config is enabled, then RA will be disabled.
    
    Change-Id: I85df0828af665d2bcc13c865b0ba40ebf882fffe
    CRs-Fixed: 1058884

--- drivers/net/wireless/qcacld-2.0/CORE/SERVICES/WMA/wma.h
+++ drivers/net/wireless/qcacld-2.0/CORE/SERVICES/WMA/wma.h
@@ -863,6 +863,7 @@ typedef struct wma_handle {
 	struct wma_runtime_pm_context runtime_context;
 	uint32_t fine_time_measurement_cap;
 	bool bpf_enabled;
+	bool bpf_packet_filter_enable;
 	bool     pause_other_vdev_on_mcc_start;
 	tSirAddonPsReq psSetting;
 }t_wma_handle, *tp_wma_handle;
