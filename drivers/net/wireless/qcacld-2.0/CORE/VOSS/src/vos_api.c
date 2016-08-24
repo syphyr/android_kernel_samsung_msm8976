c7df334736e0ac1cb2d2d013c61d70a682deb51c
Anurag Chouhan <achouhan@codeaurora.org>
2016-08-24 23:06:43 +0530

    qcacld-2.0: Disable RA filter when BPF is enabled
    In current implementation, RA filter is enabled irrespective of the
    BPF filter status. This change checks if BPF configuration in ini as
    well as in target config is enabled, then RA will be disabled.
    
    Change-Id: I85df0828af665d2bcc13c865b0ba40ebf882fffe
    CRs-Fixed: 1058884

--- drivers/net/wireless/qcacld-2.0/CORE/VOSS/src/vos_api.c
+++ drivers/net/wireless/qcacld-2.0/CORE/VOSS/src/vos_api.c
@@ -497,6 +497,8 @@ VOS_STATUS vos_open( v_CONTEXT_t *pVosContext, v_SIZE_t hddContextSize )
   macOpenParms.ssdp = pHddCtx->cfg_ini->ssdp;
   macOpenParms.enable_bcst_ptrn = pHddCtx->cfg_ini->bcastptrn;
 
+  macOpenParms.bpf_packet_filter_enable =
+               pHddCtx->cfg_ini->bpf_packet_filter_enable;
 #ifdef FEATURE_WLAN_RA_FILTERING
    macOpenParms.RArateLimitInterval = pHddCtx->cfg_ini->RArateLimitInterval;
    macOpenParms.IsRArateLimitEnabled = pHddCtx->cfg_ini->IsRArateLimitEnabled;
