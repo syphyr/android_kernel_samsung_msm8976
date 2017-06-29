9fd0d7f7aed4f94b4ad18f5f795933797628e62a
Mukul Sharma <mukul@codeaurora.org>
2017-06-29 18:25:39 +0530

    qcacld-2.0: wlan: Exit BMPS during wlan resume
    Wlan firmware locks interrupt during exit & enter BMPS.
    During wlan resume, host sends multiple commands to firmware and
    While processing of each smd commands, it exit/enter bmps mode.
    Because of much interrupt locking, BT thread get delayed and user
    experiences glitches during  a2dp streaming.
    
    As a part of this fix, Host exit BMP during starting of resume.
    Later once resume gets complete, host enter again in bmps.
    
    Change-Id: I00dac523755bcb1c3ade407cb2840a9060e23ed6
    CRs-Fixed: 2069225

--- drivers/net/wireless/qcacld-2.0/CORE/HDD/inc/wlan_hdd_main.h
+++ drivers/net/wireless/qcacld-2.0/CORE/HDD/inc/wlan_hdd_main.h
@@ -117,7 +117,7 @@
 /** Maximum time(ms)to wait for disconnect to complete **/
 #define WLAN_WAIT_TIME_DISCONNECT  5000
 #define WLAN_WAIT_TIME_STATS       800
-#define WLAN_WAIT_TIME_POWER       800
+#define WLAN_WAIT_TIME_POWER       5000
 #define WLAN_WAIT_TIME_COUNTRY     1000
 #define WLAN_WAIT_TIME_LINK_STATUS 800
 /* Amount of time to wait for sme close session callback.
