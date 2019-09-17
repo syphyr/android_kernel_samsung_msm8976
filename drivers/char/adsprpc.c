33d4e22f498dcc04db4901b32cec32a1b8dc020e
c_mtharu <mtharu@codeaurora.org>
2019-09-17 12:52:12 +0530

    [BACKPORT] msm: adsprpc: Fix integer overflow in refcount of map
    Integer overflow in refcount of map is leading to use after free. Error
    out if refcount reaches INT_MAX.
    
    Change-Id: I21e88361a8e70ef8c5c9593f1fc0ddd2b351a55a
    Acked-by: Himateja Reddy <hmreddy@qti.qualcomm.com>
    Signed-off-by: Tharun Kumar Merugu <mtharu@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1679,6 +1679,10 @@ static int fastrpc_mmap_find(struct fastrpc_file *fl, int fd, uintptr_t va,
 			if (map->va >= va &&
 					map->va + map->len <= va + len &&
 					map->fd == fd) {
+				if (map->refs + 1 == INT_MAX) {
+					spin_unlock(&me->hlock);
+					return -ETOOMANYREFS;
+				}
 				map->refs++;
 				match = map;
 				break;
@@ -1691,6 +1695,10 @@ static int fastrpc_mmap_find(struct fastrpc_file *fl, int fd, uintptr_t va,
 			if (map->va >= va &&
 					map->va + map->len <= va + len &&
 					map->fd == fd) {
+				if (map->refs + 1 == INT_MAX) {
+					spin_unlock(&fl->hlock);
+					return -ETOOMANYREFS;
+				}
 				map->refs++;
 				match = map;
 				break;
