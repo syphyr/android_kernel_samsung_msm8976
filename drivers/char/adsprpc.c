d692fb2d706e41b60a321905f91a57f939f45111
Vamsi krishna Gattupalli <vgattupa@codeaurora.org>
2020-12-02 12:59:03 +0530

    [BACKPORT] msm:ADSPRPC :Fix to avoid Use after free in fastrpc_internal_munmap
    Added a check to validate map before freeing it to avoid Use after
    free scenario.
    
    Change-Id: Ic723a4fe964a4909119663500018f2a07976105b
    Signed-off-by: Vamsi krishna Gattupalli <vgattupa@codeaurora.org>
    CVE-2021-1927
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -1651,14 +1651,19 @@ static int fastrpc_internal_munmap(struct fastrpc_file *fl,
 		mutex_unlock(&fl->map_mutex);
 		return err;
 	}
-	if (!fastrpc_mmap_remove(fl, ud->vaddrout, ud->size,
-				 &map)) {
-		VERIFY(err, !fastrpc_munmap_on_dsp(fl, map->raddr,
-				map->phys, map->size, map->flags));
-		if (err)
-			goto bail;
-		fastrpc_mmap_free(map);
+	VERIFY(err, !fastrpc_mmap_remove(fl, ud->vaddrout, ud->size, &map));
+	if (err)
+		goto bail;
+	VERIFY(err, map != NULL);
+	if (err) {
+		err = -EINVAL;
+		goto bail;
 	}
+	VERIFY(err, !fastrpc_munmap_on_dsp(fl, map->raddr,
+			map->phys, map->size, map->flags));
+	if (err)
+		goto bail;
+	fastrpc_mmap_free(map);
 bail:
 	if (err && map)
 		fastrpc_mmap_add(map);
