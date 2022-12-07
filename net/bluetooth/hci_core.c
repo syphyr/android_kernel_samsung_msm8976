b2e20281c03675a8fed6814f53ba2966f77288d8
Yang Yingliang <yangyingliang@huawei.com>
2022-12-07 10:18:34 +0800

    Bluetooth: hci_core: don't call kfree_skb() under spin_lock_irqsave()
    [ Upstream commit 39c1eb6fcbae8ce9bb71b2ac5cb609355a2b181b ]
    
    It is not allowed to call kfree_skb() from hardware interrupt
    context or with interrupts being disabled. So replace kfree_skb()
    with dev_kfree_skb_irq() under spin_lock_irqsave().
    
    Fixes: 9238f36a5a50 ("Bluetooth: Add request cmd_complete and cmd_status functions")
    Change-Id: I79f502cfa90520c23a3680171a48d63a543c6fc3
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -3495,7 +3495,7 @@ void hci_req_cmd_complete(struct hci_dev *hdev, u16 opcode, u8 status)
 		}
 
 		req_complete = bt_cb(skb)->req.complete;
-		kfree_skb(skb);
+		dev_kfree_skb_irq(skb);
 	}
 	spin_unlock_irqrestore(&hdev->cmd_q.lock, flags);
 
