57ec8e37a2aa591e689571e580546481ea96a565
Ricardo Ribalda <ribalda@chromium.org>
2024-11-14 19:10:30 +0000

    media: uvcvideo: Fix event flags in uvc_ctrl_send_events
    commit c31cffd5ae2c3d7ef21d9008977a9d117ce7a64e upstream.
    
    If there is an event that needs the V4L2_EVENT_CTRL_CH_FLAGS flag, all
    the following events will have that flag, regardless if they need it or
    not.
    
    This is because we keep using the same variable all the time and we do
    not reset its original value.
    
    Cc: stable@vger.kernel.org
    Fixes: 805e9b4a06bf ("[media] uvcvideo: Send control change events for slave ctrls when the master changes")
    Change-Id: I4c74abbf8ddedb82b3ab39b832561b206fc681da
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Link: https://lore.kernel.org/r/20241114-uvc-roi-v15-1-64cfeb56b6f8@chromium.org
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/media/usb/uvc/uvc_ctrl.c
+++ drivers/media/usb/uvc/uvc_ctrl.c
@@ -1217,13 +1217,13 @@ static void uvc_ctrl_send_events(struct uvc_fh *handle,
 {
 	struct uvc_control_mapping *mapping;
 	struct uvc_control *ctrl;
-	u32 changes = V4L2_EVENT_CTRL_CH_VALUE;
 	unsigned int i;
 	unsigned int j;
 
 	for (i = 0; i < xctrls_count; ++i) {
-		ctrl = uvc_find_control(handle->chain, xctrls[i].id, &mapping);
+		u32 changes = V4L2_EVENT_CTRL_CH_VALUE;
 
+		ctrl = uvc_find_control(handle->chain, xctrls[i].id, &mapping);
 		for (j = 0; j < ARRAY_SIZE(mapping->slave_ids); ++j) {
 			if (!mapping->slave_ids[j])
 				break;
